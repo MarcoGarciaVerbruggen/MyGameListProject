9aaffc02af0a80a18b6169e486b909530b73e105
JCaldas <1240560@isep.ipp.pt>
2026-07-13 12:25:12 +0100

    spring project setup

---
+++ gamingList/src/main/java/com/example/gaminglist/GamingListApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaminglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingListApplication.class, args);
+    }
+
+}
